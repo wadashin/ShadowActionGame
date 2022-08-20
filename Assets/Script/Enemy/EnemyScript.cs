82c088569415370ad8a4d3e7081ba6116eb13f68
wadashin <Shin.86.miso@outlook.com>
2022-08-20 22:58:40 +0900

    エネミーに移動機能をつけようとしたけどわけあってコミット

--- Assets/Script/Enemy/EnemyScript.cs
+++ Assets/Script/Enemy/EnemyScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] GameObject _player;
     void Start()
     {
         
@@ -13,6 +14,9 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(this.transform.position,_player.transform.position) < 10)
+        {
+
+        }
     }
 }
